85b10f7911c78d3e39343b4d74c086d824ea804c
Arif khan <arifkhanshanto123@gmail.com>
2022-02-05 16:57:33 +0600

    5 Feb 2022 , project submit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# multiple_password_generator
+# Multi password generator
+
+<b>Install :</b>
+```
+git clone https://github.com/arifkhan-silicornya/multiple_password_generator.git
+
+```
+
---
+++ script.js
@@ -0,0 +1,102 @@
+const PasswordAmountRange = document.getElementById('PasswordAmountRange')
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const PasswordAmountNumber = document.getElementById('PasswordAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+PasswordAmountNumber.addEventListener('input', syncPasswordAmount)
+PasswordAmountRange.addEventListener('input', syncPasswordAmount)
+
+
+function passGenerator() {
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)  
+
+  return password
+}
+
+
+let passWordArray = []
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  passWordArray = []
+  for (let index = 0; index < PasswordAmountNumber.value; index++) {
+    passWordArray.push(passGenerator())
+  }
+  passwordDisplay.innerText = passWordArray.length + " Password Generated and saved as docs";
+  
+  
+  const textToBLOB = new Blob([passWordArray], { type: 'text/plain' });
+  
+  const sFileName = 'password.docs';
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+      newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+      newLink.href = window.URL.createObjectURL(textToBLOB);
+      newLink.style.display = "none";
+      document.body.appendChild(newLink);
+  }
+
+  newLink.click(); 
+})
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+function syncPasswordAmount(e) {
+  const value = e.target.value
+  PasswordAmountNumber.value = value
+  PasswordAmountRange.value = value
+}
+
